80831d7c3260c24fbf5338ad2efdc428cdfb43ab
ivzi <irfanzid11@gmail.com>
2019-10-22 15:12:44 +0700

    Fixing link images footer

--- wp-content/themes/Travelo/footer.php
+++ wp-content/themes/Travelo/footer.php
@@ -14,30 +14,30 @@ $footer_skin = empty( $trav_options['footer_skin'] )?'style-def':$trav_options['
                         <img src="<?php echo esc_url( $logo_url ); ?>" alt="<?php bloginfo('name'); ?>" style="height: 70px"/>
                         <br><h2>0812-1111-8486</h2>
                         <a href="tel:+6281211118486">
-                            <img src="https://image.flaticon.com/icons/png/512/80/80630.png" alt="<?php bloginfo('name'); ?>" style="height: 60px"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/24.png" alt="<?php bloginfo('name'); ?>" style="height: 36px; margin-right:15px"/>
                         </a>
                         <a href="https://api.whatsapp.com/send?phone=6281211118486&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                            <img src="http://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" alt="<?php bloginfo('name'); ?>" style="height: 60px"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/whatsapp.png" alt="<?php bloginfo('name'); ?>" style="height: 36px; margin-right:15px"/>
                         </a>
                         <a href="https://api.whatsapp.com/send?phone=6281211118486&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                            <img src="http://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" alt="<?php bloginfo('name'); ?>" style="height: 60px"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/whatsapp.png" alt="<?php bloginfo('name'); ?>" style="height: 36px; margin-right:15px"/>
                         </a>
                         <a href="https://api.whatsapp.com/send?phone=6281211118486&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                            <img src="http://pngimg.com/uploads/whatsapp/whatsapp_PNG21.png" alt="<?php bloginfo('name'); ?>" style="height: 60px"/>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/whatsapp.png" alt="<?php bloginfo('name'); ?>" style="height: 36px"/>
                         </a>
                     </div>
                     <div class="col-sm-6 col-md-3">
-                            <div id="travcontactwidget-3" class="secure-transaction-content">
-                                <h2 class="widgettitle">Payment Partner</h2>
-                                <img src="wp-content/themes/Travelo/images/payment/bca.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;">
-                                <img src="wp-content/themes/Travelo/images/payment/mandiri.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;">                                
-                                <img src="wp-content/themes/Travelo/images/payment/bri.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;"><br>
-                                <img src="wp-content/themes/Travelo/images/payment/jcb.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;">
-                                <img src="wp-content/themes/Travelo/images/payment/union.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;">                                
-                                <img src="wp-content/themes/Travelo/images/payment/mc.png" style="width:70px; height:50px; margin:4px; border-radius:5px; border:1px solid gray;"><br>
-                                <img src="wp-content/themes/Travelo/images/payment/visa.png" style="width:70px; height:50px; margin:4px; border-radius:5px;border:1px solid gray;">
-                                <img src="wp-content/themes/Travelo/images/payment/american_express.png" style="width:70px; height:50px; margin:4px; border-radius:5px;">
-                            </div>
+                        <div id="travcontactwidget-3" class="secure-transaction-content">
+                            <h2 class="widgettitle">Payment Partner</h2>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/bca.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/mandiri.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;">                                
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/bri.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;"><br>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/jcb.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/union.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;">                                
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/mc.png" style="width:72px; height:48px; margin:4px; border-radius:5px; border:1px solid gray;"><br>
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/visa.png" style="width:72px; height:48px; margin:4px; border-radius:5px;border:1px solid gray;">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/payment/american_express.png" style="width:72px; height:48px; margin:4px; border-radius:5px;">
+                        </div>
                     </div>
                     <div class="col-sm-6 col-md-3">
                         <div id="nav_menu-7" class="small-box widget_nav_menu">
@@ -68,7 +68,7 @@ $footer_skin = empty( $trav_options['footer_skin'] )?'style-def':$trav_options['
                                     <a title="" href="https://www.facebook.com/Vividi-Transwisata-109946207038207" target="_blank" data-toggle="tooltip" data-original-title="facebook"><i class="soap-icon-facebook"></i></a>
                                 </li>
                                 <li class="youtube">
-                                    <a title="" href="https://www.youtube.com/" target="_blank" data-toggle="tooltip" data-original-title="youtube"><i class="soap-icon-youtube"></i></a>
+                                    <a title="" href="https://www.youtube.com/channel/UCYdywsNQA2t8mGoCB3Ovw5w" target="_blank" data-toggle="tooltip" data-original-title="youtube"><i class="soap-icon-youtube"></i></a>
                                 </li>
                                 <li class="instagram">
                                     <a title="" href="https://www.instagram.com/vividitranswisata" target="_blank" data-toggle="tooltip" data-original-title="instagram"><i class="soap-icon-instagram"></i></a>
@@ -77,8 +77,8 @@ $footer_skin = empty( $trav_options['footer_skin'] )?'style-def':$trav_options['
                                     <a title="" href="https://www.googleplus.com/vividitranswisata" target="_blank" data-toggle="tooltip" data-original-title="instagram"><i class="soap-icon-googleplus"></i></a>
                                 </li>
                             </ul><br>
-                            <img src="http://pluspng.com/img-png/get-it-on-google-play-png-get-it-on-google-play-png-519.png" style="width: 140px;margin-bottom: 5px">
-                            <img src="http://pluspng.com/img-png/download-on-app-store-png-with-without-wifi-or-data-2000.png" style="width: 140px">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/google.png" style="width: 140px; height: 45px; margin-bottom: 5px">
+                            <img src="<?php echo get_template_directory_uri(); ?>/images/app.png" style="width: 140px; height: 45px;">
                         </div>
                     </div>
                 </div>
