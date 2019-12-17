4d005c84a2eb27d74585b4878cb63f556d343ccf
fardump <farkhanfirmans@gmail.com>
2019-12-17 12:06:54 +0700

    update view hubungi kami, tambah include css vividi dashboard di index.php

--- mitra/application/views/Admin/index.php
+++ mitra/application/views/Admin/index.php
@@ -9,47 +9,26 @@ defined('BASEPATH') OR exit('No direct script access allowed');
 <html lang="en">
 <head>
     <meta charset="utf-8">
-    <title><?= $folder ?> < Vividi Transwisata</title>
+    <title><?= $folder ?> - Vividi Transwisata</title>
 
     <meta content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no" name="viewport">
-    <!-- Bootstrap 3.3.7 -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>">
-    <!-- Font Awesome -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css'); ?>">
-    <!-- Ionicons -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap/dist/css/bootstrap.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/css/dashboard-vividi.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/font-awesome/css/font-awesome.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/Ionicons/css/ionicons.min.css'); ?>">
-    <!-- Theme style -->
     <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/AdminLTE.min.css'); ?>">
-    <!-- DataTables -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'); ?>">
-    <!-- AdminLTE Skins. Choose a skin from the css/skins
-         folder instead of downloading all of them to reduce the load. -->
+    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/datatables.net-bs/css/dataTables.bootstrap.min.css'); ?>">
     <link rel="stylesheet" href="<?php echo base_url('assets/dist/css/skins/_all-skins.min.css'); ?>">
-    <!-- Morris chart -->
     <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/morris.js/morris.css'); ?>">
-    <!-- jvectormap -->
     <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/jvectormap/jquery-jvectormap.css'); ?>">
-    <!-- Date Picker -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'); ?>">
-    <!-- Daterange picker -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/bower_components/bootstrap-daterangepicker/daterangepicker.css'); ?>">
-    <!-- bootstrap wysihtml5 - text editor -->
-    <link rel="stylesheet"
-          href="<?php echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'); ?>">
-    <!-- Google Font -->
-    <link rel="stylesheet"
-          href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
-
-    <link rel="shortcut icon" href="<?php echo base_url('../wp-content/uploads/2019/09/favicon-vividi-3.png'); ?>"
-          type="image/x-icon"/>
-<!--    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">-->
+    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap-datepicker/dist/css/bootstrap-datepicker.min.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/bower_components/bootstrap-daterangepicker/daterangepicker.css'); ?>">
+    <link rel="stylesheet" href="<?php echo base_url('assets/plugins/bootstrap-wysihtml5/bootstrap3-wysihtml5.min.css'); ?>">
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Source+Sans+Pro:300,400,600,700,300italic,400italic,600italic">
+    <link rel="shortcut icon" href="<?php echo base_url('../wp-content/uploads/2019/09/favicon-vividi-3.png'); ?>" type="image/x-icon"/>
     <link rel="stylesheet" type="text/css" href="https://unpkg.com/lightpick@latest/css/lightpick.css">
 </head>
+
 <body class="hold-transition skin-blue sidebar-mini">
 <div class="wrapper">
     <!-- Header -->
--- mitra/application/views/Message/view_hubungi.php
+++ mitra/application/views/Message/view_hubungi.php
@@ -15,72 +15,93 @@
         <div class="row">
             <div class="col-md-12">
                 <div class="box box-info">
-                    <div class="box-body" style="min-height: 500px; margin-top: 20px">
-                        <div class="col-sm-3 col-md-3">
-                            <div class="box-body" style="margin-top: 5px; background-color: #F5F4F4">
-                                <span style="font-family: arial; font-size: 15px; display: inline-block; margin-left: 10px; margin-top: 10px; margin-right: 10px">Kirim Pesan Cepat</span><br>
-                                <a style="text-decoration: none;"
-                                   href="https://api.whatsapp.com/send?phone=6281211118486&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                                    <img src="https://vividi.id/wp-content/themes/Travelo/images/whatsapp.png" alt=""
-                                         style="height: 25px; margin-left: 10px; margin-right:5px; margin-top: 10px; margin-bottom: 10px"/>
-                                </a>
-                                <a style="text-decoration: none;"
-                                   href="https://api.whatsapp.com/send?phone=6287885124429&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                                    <img src="https://vividi.id/wp-content/themes/Travelo/images/whatsapp.png" alt=""
-                                         style="height: 25px; margin-right:5px;margin-top: 10px; margin-bottom: 10px"/>
-                                </a>
-                                <a style="text-decoration: none;"
-                                   href="https://api.whatsapp.com/send?phone=6285933736049&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                                    <img src="https://vividi.id/wp-content/themes/Travelo/images/whatsapp.png" alt=""
-                                         style="height: 25px; margin-right:5px;margin-top: 10px; margin-bottom: 10px"/>
-                                </a>
-                                <a style="text-decoration: none;"
-                                   href="https://api.whatsapp.com/send?phone=6281211118486&text=Silahkan hubungi Nomor Whatsapp jika ada yang ingin ditanyakan">
-                                    <img src="https://vividi.id/wp-content/themes/Travelo/images/whatsapp.png" alt=""
-                                         style="height: 25px; margin-right:5px;margin-top: 10px; margin-bottom: 10px"/>
-                                </a>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-3 col-md-3">
-                            <div class="box-body" style="margin-top: 5px; background-color: #F5F4F4">
-                                <span style="font-family: arial; font-size: 15px; display: inline-block; margin-left: 10px; margin-top: 10px; margin-right: 10px">Bantuan Melalui Email</span><br>
-                                <img src="https://vividi.id/wp-content/themes/Travelo/images/email.png" alt=""
-                                     style="margin-left: 10px; height: 25px; margin-right:5px;margin-top: 10px; margin-bottom: 10px"/>
-                                <span style="text-decoration: none;font-family: arial; display: inline-block; margin-top: 20px; margin-right: 20px;  float: right">info@vividi.id</span>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-3 col-md-3">
-                            <div class="box-body" style="margin-top: 5px; background-color: #F5F4F4">
-                                <span style="font-family: arial; font-size: 15px; display: inline-block; margin-left: 20px; margin-top: 10px; margin-right: 15px">Bantuan Telepon</span><br>
-                                <img src="https://vividi.id/wp-content/themes/Travelo/images/telephone.jpg" alt=""
-                                     style="height: 25px; margin-left: 10px; margin-right:5px;margin-top: 10px; margin-bottom: 10px"/>
-                                <a style="text-decoration: none; display: inline-block; margin-top: 20px; margin-right: 20px;  float: right"
-                                   href="tel:+623414382253">+62 341 438 2253</a>
-                            </div>
-                        </div>
-
-                        <div class="col-sm-3 col-md-3">
-                            <div class="box-body" style="margin-top: 5px; background-color: #F5F4F4">
-                                <span style="font-family: arial; font-size: 15px; display: inline-block; margin-left: 20px; margin-top: 10px; margin-right: 15px">Bantuan mendesak 24 jam</span><br>
-                                <img src="https://vividi.id/wp-content/themes/Travelo/images/telephone.jpg" alt=""
-                                     style="height: 25px; margin-left: 10px;margin-top: 10px; margin-bottom: 10px"/>
-                                <a style="text-decoration: none; display: inline-block; margin-top: 20px; margin-right: 30px;  float: right"
-                                   href="tel:+6281211118486">+62 812 1111 8486</a>
+                    <div class="box-body box-hubungi">
+                        <div class="row">
+                            <div class="col-md-12 space-med">
+                                <span class="txt-hubungi-desc">Hubungi kami melalui contact yang tersedia dibawah ini</span>
                             </div>
                         </div>
                         <div class="row">
-                            <div class="col-sm-12 col-md-12" style="text-align: center">
-                                <hr>
-                                <span style="font-family: arial; font-size: 15px; display: inline-block; margin-top: 10px; margin-bottom: 10px;">© 2019 PT. Vividi Transindo Utama</span>
+                            <div class="col-sm-3 col-md-3 space-small">
+                                <div class="info-box">
+                                    <span class="info-box-icon">
+                                        <a href="https://api.whatsapp.com/send?phone=6281211118486&text=Halo Admin Vividi, ada yang ingin saya tanyakan">
+                                            <i class="fa fa-whatsapp"></i>
+                                        </a>
+                                    </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text info-txt">Hubungi via Whatsapp</span><br>
+                                        <span class="info-box-number info-txt-nomor">
+                                            <a href="https://api.whatsapp.com/send?phone=6281211118486&text=Halo Admin Vividi, ada yang ingin saya tanyakan" class="btn btn-primary btn-contact-us">
+                                                +62 812 1111 8486
+                                            </a>
+                                        </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3 col-md-3 space-small">
+                                <div class="info-box">
+                                    <span class="info-box-icon">
+                                        <a href=mailto:info@vividi.id?subject="Halo Admin Vividi, ada yang ingin saya tanyakan">
+                                            <i class="fa fa-envelope-o"></i>
+                                        </a>
+                                    </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text info-txt">Hubungi via Email</span><br>
+                                        <span class="info-box-number info-txt-nomor">
+                                            <a href=mailto:info@vividi.id?subject="Halo Admin Vividi, ada yang ingin saya tanyakan" class="btn btn-primary btn-contact-us">
+                                                info@vividi.id
+                                            </a>
+                                        </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3 col-md-3 space-small">
+                                <div class="info-box">
+                                    <span class="info-box-icon">
+                                        <a href="tel:+623414382253">
+                                            <i class="fa fa-phone"></i>
+                                        </a>
+                                    </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text info-txt">Hubungi via Telepon</span><br>
+                                        <span class="info-box-number info-txt-nomor">
+                                            <a href="tel:+623414382253" class="btn btn-primary btn-contact-us">
+                                                +62 341 438 2253
+                                            </a>
+                                        </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
+                            </div>
+
+                            <div class="col-sm-3 col-md-3 space-small">
+                                <div class="info-box">
+                                    <span class="info-box-icon">
+                                        <a href="tel:+6281211118486">
+                                            <i class="fa fa-phone"></i>
+                                        </a>
+                                    </span>
+                                    <div class="info-box-content">
+                                        <span class="info-box-text info-txt">Support Online 24 Jam</span><br>
+                                        <span class="info-box-number info-txt-nomor">
+                                            <a href="tel:+6281211118486" class="btn btn-primary btn-contact-us">
+                                                +62 812 1111 8486
+                                            </a>
+                                        </span>
+                                    </div>
+                                    <!-- /.info-box-content -->
+                                </div>
                             </div>
                         </div>
                     </div>
                 </div>
                 <!-- /.box -->
             </div>
-            <!-- /.col-->
         </div>
         <!-- ./row -->
     </section>
